571254e20432a7f717f69e2967ad78667a8ef655
Jake Schroeder <jake.schroeder@isophex.com>
2024-06-01 20:38:11 +0100

    feat: controls indicator

--- src/components/HUD/WASDInstructions.tsx
+++ src/components/HUD/WASDInstructions.tsx
@@ -1,6 +1,7 @@
 export function WASDInstructions() {
   return (
-    <div className="z-50 absolute bottom-8 left-8">
+    <div className="flex flex-col justify-center items-center space-y-4 z-50 absolute bottom-8 left-8">
+      <h4 className="text-white">Controls:</h4>
       <img
         src="/images/wasd.png"
         alt="WASD"
