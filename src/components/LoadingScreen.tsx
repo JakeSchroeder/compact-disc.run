fcc503f15e9c1c31d431036818ae18bd2d1bf25d
Jake Schroeder <jake.schroeder@isophex.com>
2024-06-23 15:30:06 +0100

    fix: misc issues

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useSceneStore } from "../stores/SceneStore";
-import round from "lodash/round";
 
 export function LoadingScreen() {
   const sceneLoading = useSceneStore((state) => state.sceneLoading);
